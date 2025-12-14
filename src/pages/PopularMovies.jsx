e8cd5b8a1c9c3946da5245b501d14d3d6cfefbcc
LukasKrasny <1.lukaskrasny@gmail.com>
2025-12-14 19:42:50 +0100

    Integration of pagination into page

--- src/pages/PopularMovies.jsx
+++ src/pages/PopularMovies.jsx
@@ -1,9 +1,17 @@
 import useFetchMovies from "../hooks/useFetchMovies";
 import MovieCard from "../components/MovieCard";
+import Pagination from "../components/Pagination";
 import { Link } from "react-router-dom";
 
 const PopularMovies = () => {
-  const { data, loading, error } = useFetchMovies();
+  const { 
+    movies, 
+    loading, 
+    error,
+    currentPage,
+    totalPages,
+    setCurrentPage
+   } = useFetchMovies()
 
   if (loading) return <div className="loading">Načítání...</div>;
   if (error) return <div>Chyba: {error.message}</div>;
@@ -13,10 +21,29 @@ const PopularMovies = () => {
       <h1 className="text-5xl m-10 text-center text-tawny-olive font-bold">
         Populární filmy
       </h1>
+
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      )}
+
+      {/* MŘÍŽKA S FILMY */}
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-        {data &&
-          data.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        {!loading &&
+          !error &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
+
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      )}
     </div>
   );
 };
