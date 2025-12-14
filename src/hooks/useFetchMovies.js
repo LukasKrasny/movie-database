ff6efa2941016c3e780167ebfd08d18d51ff7283
LukasKrasny <1.lukaskrasny@gmail.com>
2025-12-14 18:36:48 +0100

    Custom hook adjustment

--- src/hooks/useFetchMovies.js
+++ src/hooks/useFetchMovies.js
@@ -2,17 +2,32 @@ import { useState, useEffect } from "react"
 import { fetchPopularMovies } from "../api/tmdb"
 
 const useFetchMovies = () => {
-  const [data, setData] = useState(null)
+  const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
 
   useEffect(() => {
-    fetchPopularMovies()
-      .then(movies => setData(movies))
-      .catch(err => setError(err))
-      .finally(() => setLoading(false))
-  }, [])
+    setLoading(true)
+    setError(null)
 
-  return {data, loading, error} 
-}
+    const fetchMovies = async () => {
+      try {
+        const data = await fetchPopularMovies(currentPage)
+        setMovies(data.results)
+        setTotalPages(data.total_pages)
+      } catch (err) {
+        setError(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchMovies()
+  } , [currentPage])
+
+  return { movies, loading, error, currentPage, totalPages, setCurrentPage }
+} 
+
 export default useFetchMovies
