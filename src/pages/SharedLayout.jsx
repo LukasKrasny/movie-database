83c3f15c33be482c59eaab6e85fb2f24d87e9e9d
LukasKrasny <1.lukaskrasny@gmail.com>
2025-12-30 21:12:28 +0100

    Moving CSS classes from App.jsx

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -2,11 +2,10 @@ import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 
 const SharedLayout = () => {
-  return <>
+  return <div className="bg-black-wash min-h-screen">
       <Navbar />
       <Outlet />
-    </>
-
+    </div>
 }
 
 export default SharedLayout
