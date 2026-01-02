2d55c2c11807521d3a5ce9326e31cd5b802a2727
LukasKrasny <1.lukaskrasny@gmail.com>
2026-01-02 15:15:43 +0100

    Creating a navigation bar with a responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,84 @@
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { Film, Menu, X, House } from "lucide-react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  }
   return (
-    <header>
-      <nav>
-        <NavLink
+    <header className="bg-black-wash/95 border-b border-tawny-olive/50 shadow-xl shadow-corn-harvest/30 p-3 fixed w-full top-0 left-0 z-50">
+      <div className="mx-auto flex justify-between items-center">
+        <Link
           to="/"
-          className={
-            ({ isActive }) =>
+          className="text-4xl font-bold text-tawny-olive flex items-center gap-2"
+        >
+          <Film className="w-8 h-8" /> MovieDB
+        </Link>
+        <nav className="hidden md:flex items-center space-x-4">
+          <NavLink
+            to="/"  
+            className={({ isActive }) =>
               isActive
-                ? "text-tawny-olive font-bold border-b-2 border-tawny-olive" // Aktivní (zlatá)
-                : "text-gray-400 hover:text-white" // Neaktivní (šedá)
+                ? "text-tawny-olive font-bold bg-corn-harvest/20 py-2 px-2 rounded-lg transition-colors duration-200"
+                : "text-tawny-olive hover:bg-corn-harvest/20 py-2 px-2 rounded-lg transition-colors duration-200"
+            }
+          >
+            <House className="w-6 h-6" />
+          </NavLink>
+          <NavLink
+            to="/popular"
+            className={({ isActive }) =>
+              isActive
+                ? "text-tawny-olive font-bold bg-corn-harvest/20 py-2 px-2 rounded-lg transition-colors duration-200"
+                : "text-tawny-olive hover:bg-corn-harvest/20 py-2 px-2 rounded-lg transition-colors duration-200"
+            }
+          >
+            Populární filmy
+          </NavLink>
+        </nav>
+
+        <button
+          onClick={toggleMenu}
+          className="md:hidden focus:outline-none z-50"
+        >
+          {isMenuOpen ? (
+            <X size={32} className="text-tawny-olive cursor-pointer" />
+          ) : (
+            <Menu size={32} className="text-tawny-olive cursor-pointer" />
+          )}
+        </button>
+      </div>
+      {isMenuOpen && (
+        <div className="absolute top-0 left-0 w-full h-screen bg-black-wash/80 flex flex-col items-center justify-center space-y-8 md:hidden backdrop-blur-sm z-40">
+          <NavLink
+            to="/"
+            onClick={closeMenu}
+            className={({ isActive }) =>
+              isActive
+                ? "text-tawny-olive font-bold bg-corn-harvest/20 py-2 px-2 rounded-lg transition-colors duration-200 text-2xl"
+                : "text-tawny-olive font-bold hover:bg-corn-harvest/20 py-2 px-2 rounded-lg transition-colors duration-200 text-2xl"
           }
         >
           Domů
         </NavLink>
         <NavLink
           to="/popular"
-          className={
-            ({ isActive }) =>
+            onClick={closeMenu}
+            className={({ isActive }) =>
               isActive
-                ? "text-tawny-olive font-bold border-b-2 border-tawny-olive" // Aktivní (zlatá)
-                : "text-gray-400 hover:text-white" // Neaktivní (šedá)
+                ? "text-tawny-olive font-bold bg-corn-harvest/20 py-2 px-2 rounded-lg transition-colors duration-200 text-2xl"
+                : "text-tawny-olive font-bold hover:bg-corn-harvest/20 py-2 px-2 rounded-lg transition-colors duration-200 text-2xl"
           }
         >
           Populární filmy
         </NavLink>
-      </nav>
+        </div>
+      )}
     </header>
   );
 };
