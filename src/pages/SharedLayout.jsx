5284d5adebf55ed69fa2cc6c2131756929ba64f8
LukasKrasny <1.lukaskrasny@gmail.com>
2026-01-03 12:28:27 +0100

    Adding a Footer to SharedLayout

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const SharedLayout = () => {
   return <div className="bg-black-wash min-h-screen">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
 }
 
