a4abc47ad1de5d16e55473c3ee513671c4986d2f
LukasKrasny <1.lukaskrasny@gmail.com>
2026-01-03 12:26:59 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="bg-scarabeus-sacer border-t border-tawny-olive/50 p-4 mt-auto">
+      <div className="max-w-7xl mx-auto text-center space-y-2">
+        <p className="text-center text-lg font-medium text-tawny-olive">
+          &copy; {new Date().getFullYear()} Lukáš Krásný - MovieDB. Všechna práva vyhrazena.
+        </p>
+        <p className="text-center text-tawny-olive">
+          Data poskytována přes TMDB API. Tento projekt slouží pro výukové
+          účely.
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
