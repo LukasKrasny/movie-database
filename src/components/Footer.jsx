74b10028ceb255daa12f48006a6ce82a85feb9cd
LukasKrasny <1.lukaskrasny@gmail.com>
2026-01-03 12:37:53 +0100

    Background color adjustment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-scarabeus-sacer border-t border-tawny-olive/50 p-4 mt-auto">
+    <footer className="bg-scarabeus-sacer/50 border-t border-tawny-olive/50 p-4 mt-auto">
       <div className="max-w-7xl mx-auto text-center space-y-2">
         <p className="text-center text-lg font-medium text-tawny-olive">
           &copy; {new Date().getFullYear()} Lukáš Krásný - MovieDB. Všechna práva vyhrazena.
